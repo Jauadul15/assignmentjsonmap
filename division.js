d2a20d7bb526671e077d368e159bd54875f04627
Jauadul15 <rashik.gmh@gmail.com>
2022-11-08 13:39:26 +0600

    assignmentnew

---
+++ division.js
@@ -0,0 +1,11 @@
+let divisionlist=[
+    {name: 'Dhaka', shortnm: 'DK'},
+    {name: 'Chittagong', shortnm: 'CTG'},
+    {name: 'Rajshahi', shortnm: 'RJ'},
+    {name: 'Khulna', shortnm: 'KL'},
+    {name: 'Barishal', shortnm: 'BR'},
+    {name: 'Sylhet', shortnm: 'SYL'},
+   
+]
+
+export {divisionlist};
---
+++ index.js
@@ -0,0 +1,7 @@
+import {divisionlist} from "./division.js";
+
+divisionlist.map((iteam,i)=>
+{
+    let Alldivisions=`Bangladesh Divisions are ${iteam.name}`
+    console.log(Alldivisions)
+})
